32fd59ad614ec8e8d2fa788b635e17ece488bf72
yjny <144751566+yjny@users.noreply.github.com>
2023-10-23 19:32:42 +0900

    2주차 과제

--- Week02_yjny/src/Pizza.java
+++ Week02_yjny/src/Pizza.java
@@ -1,9 +1,14 @@
 
+/*Week02_01*/
+
+class Circle {
+	protected int radius;
+	public Circle (int r) { radius = r;}
+}
 public class Pizza {
 
 	public static void main(String[] args) {
-		// Week02_01
-		//과자ㅔ 2까지 해서 링크제출, 평가X
+		
 
 	}
 
